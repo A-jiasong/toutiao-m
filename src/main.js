7fd5352ed76be97b5cf10547ef19eb9531100a20
宋佳 <1370402406@qq.com>
2021-02-22 11:33:13 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import './styles/index.less'
+// 导入移动端vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
