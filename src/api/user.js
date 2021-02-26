6996e4ae09abd1df5c23607728ba51bf6c8bde3f
宋佳 <1370402406@qq.com>
2021-02-26 11:51:02 +0800

    05-设置请求拦截器

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,6 @@
 // 封装用户相关的请求模块
 import request from '@/utils/request'
 
-// 引入store，来获取token数据
-import store from '@/store'
-
 // 用户登录
 export const login = data => {
   return request({
@@ -25,12 +22,12 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
+    url: '/app/v1_0/user'
     // 发送请求头数据
-    headers: {
-      // 该接口需要授权才能访问
-      // token的数据格式：Bearer token数据，Bearer后面有一个空格
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    // 该接口需要授权才能访问
+    // token的数据格式：Bearer token数据，Bearer后面有一个空格
+    // Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
