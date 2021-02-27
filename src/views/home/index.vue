2aa7b476e273981bb9924fe82d0062dec61e374c
宋佳 <1370402406@qq.com>
2021-02-27 19:06:04 +0800

    显示列表项内容

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar">
+    <van-nav-bar class="page-nav-bar" fixed>
       <van-button
         class="search-btn"
         slot="title"
@@ -14,12 +14,14 @@
     </van-nav-bar>
     <!-- 频道列表 -->
     <van-tabs class="channel-tabs" v-model="active" animated swipeable>
-      <van-tab title="标签 1">内容 1</van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
-      <van-tab title="标签 3">内容 3</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
-      <van-tab title="标签 1">内容 1</van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab
+        :title="channel.name"
+        v-for="channel in channels"
+        :key="channel.id"
+      >
+        <!-- 频道的文章列表 子组件-->
+        <article-list :channel="channel" />
+      </van-tab>
       <!-- 放一个空的插槽，当tab移到最右边时，不至于会把最后一个挡住 -->
       <div slot="nav-right" class="placeholder"></div>
       <div slot="nav-right" class="hamburger-btn">
@@ -30,25 +32,40 @@
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+import ArticleList from './components/article-list.vue'
+
 export default {
   name: 'homePage',
-  components: {},
+  components: { ArticleList },
   props: {},
   data() {
     return {
-      active: 0
+      active: 0,
+      channels: [] // 频道列表
     }
   },
   computed: {},
   watch: {},
-  created() {},
+  created() {
+    this.loadChannel()
+  },
   mounted() {},
-  methods: {}
+  methods: {
+    async loadChannel() {
+      try {
+        const { data } = await getUserChannels()
+        this.channels = data.data.channels
+      } catch (err) {}
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
 .home-container {
+  padding-top: 174px;
+  padding-bottom: 100px;
   // 当组件中加了scoped 对内部样式的修改需要加 /deep/ ，或者去掉scoped
   /deep/ .van-nav-bar__title {
     max-width: unset;
@@ -68,6 +85,11 @@ export default {
   }
   /deep/ .channel-tabs {
     .van-tabs__wrap {
+      position: fixed;
+      top: 92px;
+      z-index: 1;
+      left: 0;
+      right: 0;
       height: 82px;
     }
     .van-tab {
