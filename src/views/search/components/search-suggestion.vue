5bfbb585894cae315ddff2c56d07305f631215e4
宋佳 <1370402406@qq.com>
2021-03-02 18:29:55 +0800

    02-防抖优化lodash-debounce

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -11,6 +11,8 @@
 
 <script>
 import { getSearchSuggestion } from '@/api/search.js'
+// lodash 支持按需加载，有利于打包结果优化
+import { debounce } from 'lodash'
 
 export default {
   name: 'searchSuggestion',
@@ -30,9 +32,16 @@ export default {
   watch: {
     // 监听输入框的变化
     searchValue: {
-      handler(val) {
+      // debounce 函数
+      // 参数1：函数
+      // 参数2：防抖时间
+      // 返回值：防抖之后的函数，和参数1功能是一样的
+      handler: debounce(function(val) {
         this.loadSearchSuggestion(val)
-      },
+      }, 1000),
+      // handler(val) {
+      //   this.loadSearchSuggestion(val)
+      // },
       // 首次监视触发
       immediate: true
     }
