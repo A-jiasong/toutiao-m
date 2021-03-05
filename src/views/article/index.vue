d9e2b1ab4bf0b94e5e1672b4c95c9f5fade09d56
宋佳 <1370402406@qq.com>
2021-03-05 17:08:03 +0800

    6.3-文章收藏

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -75,6 +75,23 @@
           ref="article-content"
         ></div>
         <van-divider>正文结束</van-divider>
+        <!-- 底部区域 -->
+        <div class="article-bottom">
+          <van-button class="comment-btn" type="default" round size="small"
+            >写评论</van-button
+          >
+          <van-icon name="comment-o" badge="123" color="#777" />
+          <!-- 收藏组件 -->
+          <collect-article
+            class="btn-item"
+            v-model="article.is_collected"
+            :articleId="article.art_id"
+          />
+          <!-- <van-button class="btn-item" icon="star-o" /> -->
+          <van-button class="btn-item" icon="good-job-o" />
+          <van-icon name="share" color="#777777"></van-icon>
+        </div>
+        <!-- /底部区域 -->
       </div>
       <!-- /加载完成-文章详情 -->
 
@@ -93,18 +110,6 @@
       </div>
       <!-- /加载失败：其它未知错误（例如网络原因或服务端异常） -->
     </div>
-
-    <!-- 底部区域 -->
-    <div class="article-bottom">
-      <van-button class="comment-btn" type="default" round size="small"
-        >写评论</van-button
-      >
-      <van-icon name="comment-o" badge="123" color="#777" />
-      <van-icon color="#777" name="star-o" />
-      <van-icon color="#777" name="good-job-o" />
-      <van-icon name="share" color="#777777"></van-icon>
-    </div>
-    <!-- /底部区域 -->
   </div>
 </template>
 
@@ -112,11 +117,13 @@
 import { getArticleById } from '@/api/article'
 import { ImagePreview } from 'vant'
 import FollowedUser from '@/components/followed-user/index'
+import CollectArticle from '@/components/collect-article'
 
 export default {
   name: 'articleIndex',
   components: {
-    FollowedUser
+    FollowedUser,
+    CollectArticle
   },
   props: {
     articleId: {
@@ -287,12 +294,25 @@ export default {
       line-height: 46px;
       color: #a7a7a7;
     }
-    .van-icon {
+    /deep/ .van-icon {
       font-size: 40px;
       .van-info {
         font-size: 16px;
         background-color: #e22829;
       }
+    }
+    .btn-item {
+      border: none;
+      padding: 0;
+      height: 40px;
+      line-height: 40px;
+      color: #777;
+    }
+    .collect-btn--collected {
+      color: #ffa500;
+    }
+    .like-btn--liked {
+      color: #e5645f;
     }
   }
 }
