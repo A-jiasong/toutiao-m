7962a74526adfa1a774297e318bc09036bd76e1a
宋佳 <1370402406@qq.com>
2021-03-07 21:16:34 +0800

    编辑用户资料

--- src/api/user.js
+++ src/api/user.js
@@ -58,3 +58,29 @@ export const deleteFollow = target => {
     url: `/app/v1_0/user/followings/${target}`
   })
 }
+
+// 获取用户自己的信息
+export const getUserProfile = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/profile'
+  })
+}
+
+// 更新用户资料
+export const updateUserProfile = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/profile',
+    data
+  })
+}
+
+// 更新用户头像
+export const updateUserPhoto = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/photo',
+    data
+  })
+}
